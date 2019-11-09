afec7d21c5c369764dbdbbdf447e1bd996ea1b94
mbrookesy <milobrookes0@gmail.com>
2019-11-09 17:44:57 +0000

    New clear function added

---
+++ src/main/java/main/Battle.java
@@ -0,0 +1,55 @@
+package main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Battle {
+
+	public void battle(Scanner sc, Character mc, String zone) {
+		Random randomGen = new Random();
+		RandomMon ranmon = new RandomMon();
+		
+		String condition = "";
+		
+		Monster mon = ranmon.ranMon(zone);
+
+		System.out.println("Suddenly a lvl " + mon.getLvl() + " " + mon.getName() + " appeared");
+
+		do {
+			int accuracy = 100;
+			System.out.println("What do you do? (Input Number)");
+			System.out.println("1) Attack");
+			System.out.println("2) Spells");
+			System.out.println("3) Guard");
+			System.out.println("4) Items");
+			System.out.println("5) Flee");
+			System.out.println();
+			int action = sc.nextInt();
+
+			if (action == 1) {
+				accuracy = randomGen.nextInt(100);
+				if (accuracy < 5) {
+					System.out.println("attack missed");
+				} else {
+					System.out.println(mc.getName() + " did " + mc.getAtt() + " damage to the devil! ");
+					mon.setHp(mon.getHp() - mc.getAtt());
+					System.out.println("The devil now has only " + mon.getHp() + "hp");
+				}
+
+			} else if (action == 2) {
+
+			} else if (action == 3) {
+
+			} else if (action == 4) {
+
+			} else if (action == 5) {
+				System.out.println(mc.getName() + " climbed on his bottle of pepsi max and used it as a rocket to flee");
+				System.out.println("");
+				condition = "flee";
+			}
+
+		} while (mon.getHp() > 0 && mc.getHp() > 0 && condition != "flee");
+
+	}
+
+}
---
+++ src/main/java/main/Start.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Start {
+
+	public Character begin(Scanner sc) {
+		System.out.println("Jordan...");
+		sc.nextLine();
+		System.out.println("What is your name?");
+		String name = sc.nextLine();
+		Character mainChar = new Character(name);
+		System.out.println("Hello " + name + " let us begin.");
+		
+		return mainChar;
+	}
+
+}
