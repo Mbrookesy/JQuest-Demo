31505b7424447a7535b90825cdcf02d997739991
mbrookesy <milobrookes0@gmail.com>
2019-11-09 17:53:25 +0000

    bug fixes for alpha version

---
+++ src/main/java/main/App.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Start st = new Start();
+		Game games = new Game();
+
+		Character mainChar = st.begin(sc);
+		games.standby(sc, mainChar);
+		
+		
+
+	}
+
+}
