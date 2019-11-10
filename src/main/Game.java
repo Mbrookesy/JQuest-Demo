3d55ee448441b43f1e8cd683fa562538c82967be
mbrookesy <milobrookes0@gmail.com>
2019-11-10 13:53:18 +0000

    Rebuilt project

---
+++ src/main/Game.java
@@ -0,0 +1,36 @@
+package main;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+import main.Battle;
+import main.Character;
+
+public class Game {
+	
+	public void standby(Scanner sc, Character mc) {
+		Battle btl = new Battle();
+		Random randomGen = new Random();
+		int pass = 0;
+		String zone = "forest";
+		
+		System.out.println();
+		System.out.println("You begin to walk, press enter to continue walking");
+		
+		do {
+			pass = randomGen.nextInt(100);
+			System.out.println("Step...");
+			if (pass > 50) {
+				System.out.println("....!");
+				btl.battle(sc, mc, zone);
+			}
+			System.out.println("...");
+			
+		}while(pass < 90);
+		
+		System.out.println("You made it out of the forest");
+		
+	}
+
+}
